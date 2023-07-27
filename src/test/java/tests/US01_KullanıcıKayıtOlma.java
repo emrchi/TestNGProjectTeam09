7630cc21b102d87ff40f34eafaf0c014d5013967
Semanur Malkoç <semamalkoc01@gmail.com>
2023-07-28 00:43:28 +0300

    semaV2

--- src/test/java/tests/US01_KullanıcıKayıtOlma.java
+++ src/test/java/tests/US01_KullanıcıKayıtOlma.java
@@ -1,4 +1,12 @@
 package tests;
 
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class US01_KullanıcıKayıtOlma {
+    @Test
+    public void test01() {
+        Driver.getDriver().get(ConfigReader.getProperty(""));
+    }
 }
